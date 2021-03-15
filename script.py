21e0906ecba5d66058e2c06af4a860e7169eccb4
rajan.singh2 <rajan.singh@cerebry.co>
2021-03-15 23:58:43 +0530

    added the script file

---
+++ script.py
@@ -0,0 +1,14 @@
+from string import ascii_lowercase
+print('3idiots')
+print("5pointssomeone")
+print("4")
+print("read")
+print("password")
+print("c")
+file = open("/home/rajan/Documents/Break-DES-6-round-using-chosen-plain-text-attack-master/input_random.txt",'r+')
+for i in range(0,200001):
+# for i in range(0,10):
+  print(file.readline())
+  print("c")
+print("back")
+print("exit")
